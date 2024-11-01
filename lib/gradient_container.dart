1f0f084bfc97af7a0a48ed082fffa2a6053dcd0d
Md. Refayatul Islam <refayatul7@gmail.com>
2024-11-01 18:46:25 +0600

    maded some more change like creating new class

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:eureka/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -16,15 +17,7 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: Text(
-          'Alcohol',
-          style: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      child: const Center(child: StyledText()),
     );
   }
 }
