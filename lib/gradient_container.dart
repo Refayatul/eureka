96ead2f6622df16fb6b699f1e609d011524d4592
Md. Refayatul Islam <refayatul7@gmail.com>
2024-11-07 15:02:02 +0600

    fix some error

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:eureka/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 255, 105, 180),
             Color.fromARGB(255, 0, 190, 255),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
@@ -22,7 +25,7 @@ class GradientContainer extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             StyledText(),
-            Text('hi'),
+            Text('Dreams'),
           ],
         ),
       ),
